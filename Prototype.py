c1d4443f93df93554a8047a0141999933958f2f0
IsabelSaylor <IsabelSaylor@proton.me>
2026-03-14 15:07:20 -0400

    LOGS
    -Basic Movement

--- Prototype.py
+++ Prototype.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 """
 Simulated Map
     1 = Wall, 
@@ -15,3 +17,31 @@ TestField = [
     [1, 1, 1, 1, 1, 1]
 ]
 
+iteration = 0
+player = 2
+
+for tile in TestField:
+    print(tile)
+
+for row_index, row in enumerate(TestField):
+    for col_index, tile in enumerate(row):
+        if tile == 2:
+            print("Player at: ", row_index, col_index)
+
+            target_row = row_index
+            target_col = col_index + 1
+            TestField[row_index][col_index] = 0
+
+            time.sleep(1)
+            if TestField[target_row][target_col] == 1:
+                print("Move Gets Blocked and Redrects.")
+                TestField[row_index][col_index] = 2
+                break
+
+            TestField[target_row][target_col] = 2
+            print("Player at: ", target_row, target_col)
+            break
+            
+
+for tile in TestField:
+    print(tile)
